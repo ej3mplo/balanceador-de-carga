1220fce9233ed7712a8277cd4027be1f7fcf3c22
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2019-12-06 18:53:45 -0600

    Change to read environment variables

--- nodo/index.php
+++ nodo/index.php
@@ -12,5 +12,10 @@ echo '<h3> Host: ' . $_SERVER['SERVER_NAME'] . '</h3><br>';
 echo '<h3> IP del cliente: ' . $_SERVER['REMOTE_ADDR'] . '</h3><br>';
 echo '<h3> No. de visitas: ' . $_SESSION['counter'] . '</h3><br>';
 
+echo '<h1>Variables de entorno</h1>';
+echo '<h3>'. getenv('TEST_VALUE') .'</h3>';
+echo '<h3>'. getenv('TEST_VALUE1') .'</h3>';
+echo '<h3>'. getenv('TEST_VALUE2') .'</h3>';
+
 echo '<h4>Datos completos del servidor</h4>';
 echo '<pre>' . print_r($_SERVER, true);
